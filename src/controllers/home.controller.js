41ea90813dc416ade1b1b6842872d4bc56727125
Neelam <neelam0101singh@gmail.com>
2022-06-06 23:27:07 +0530

    pagination added

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -5,13 +5,25 @@ const router=express.Router()
 
 router.get("", async (req, res) => {
     try {
-      const home = await Home.find().lean().exec();
-     return res.status(200).send({ home: home }); // []
+        const page=req.query.page||1;
+        const pagesize=req.query.pagesize||10
+        const skip=(page-1)*pagesize
+        //  const filter1=req.query.filter
+        //  const filtervalue=req.query.filtervalue
+        // console.log("abdgehfwhhdkhb",filter1,filtervalue)
+       
+        //  if(filter1){
+            
+        //      console.log("aa na ",{[filter1]:filtervalue})
+        //  let home = await Home.find({[filter1]:filtervalue}).lean().exec();
+        //  return res.status(200).send({ home: home }) ;
+        // }
+        let home = await Home.find().skip(skip).limit(pagesize).lean().exec();
+     return res.status(200).send({ home: home }) ; // []
     } catch (err) {
       return res.status(500).send({ message: err.message });
     }
   });
-
 
  
 
